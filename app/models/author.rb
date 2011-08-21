b500d4a88b830a71d48962cca5160deabd462466
derekcheung <derek.cheung@transact24.com>
2011-08-21 12:38:59 +0800

    test upload

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,6 @@
 class Author < ActiveRecord::Base
+  has_many :attachments, :as => :parent, :order => 'created_at desc', 
+    :dependent => :destroy, :class_name => "::Attachment"
+  
+  mount_uploader :photo, AttachmentUploader
 end
