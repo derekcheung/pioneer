2d454b1184019caa1b3469934d2e0c149b2553a3
derekcheung <derek.cheung@transact24.com>
2011-09-21 23:37:58 +0800

    edit flash

--- public/javascripts/js.js
+++ public/javascripts/js.js
@@ -37,16 +37,35 @@ function banner(width,height,pics,links,texts) { //v2.0
 	var focus_height=height
 	var text_height=0
 	var swf_height = focus_height+text_height
+	var pics=pics
+	var links=links
+	var texts=texts
+	var config = '3|0xffffff|0x000000|50|0xffffff|0x000000|0x000000';
+    var texts = '';
 
-var pics=pics
-var links=links
-var texts=texts
-	
-	document.write('<object ID="focus_flash" classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,0,0" width="'+ focus_width +'" height="'+ swf_height +'">');
+	document.write('<object ID="focus_flash" classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,0,0" width="100%" height="'+ swf_height +'">');
 	document.write('<param name="allowScriptAccess" value="always"><param name="movie" value="sitemap/pixviewer.swf"><param name="quality" value="high"><param name="bgcolor" value="#E7E7E7">');
 	document.write('<param name="menu" value="false"><param name=wmode value="opaque">');
-	document.write('<param name="FlashVars" value="pics='+pics+'&links='+links+'&texts='+texts+'&borderwidth='+focus_width+'&borderheight='+focus_height+'&textheight='+text_height+'">');
-	document.write('<embed ID="focus_flash" src="sitemap/pixviewer.swf" wmode="opaque" FlashVars="pics='+pics+'&links='+links+'&texts='+texts+'&borderwidth='+focus_width+'&borderheight='+focus_height+'&textheight='+text_height+'" menu="false" bgcolor="#E7E7E7" quality="high" width="'+ focus_width +'" height="'+ focus_height +'" allowScriptAccess="always" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" />');		document.write('</object>');
+	// document.write('<param name="FlashVars" value="pics='+pics+'&links='+links+'&texts='+texts+'&borderwidth='+focus_width+'&borderheight='+focus_height+'&textheight='+text_height+'">');
+	// document.write('<embed ID="focus_flash" src="sitemap/focus.swf" wmode="opaque" FlashVars="pics='+pics+'&links='+links+'&texts='+texts+'&borderwidth='+focus_width+'&borderheight='+focus_height+'&textheight='+text_height+'" menu="false" bgcolor="#E7E7E7" quality="high" width="100%" height="'+ focus_height +'" allowScriptAccess="always" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" />');
+	document.write('<param name="FlashVars" value="config='+config+'&bcastr_flie='+pics+'&bcastr_link='+links+'&bcastr_title='+texts+'&borderheight='+focus_height+'&textheight='+text_height+'" />');
+	document.write('<embed ID="focus_flash" src="sitemap/focus.swf" wmode="opaque" FlashVars="config='+config+'&bcastr_flie='+pics+'&bcastr_link='+links+'&bcastr_title='+texts+'" menu="false" quality="high" width="100%" height="'+ swf_height +'&borderheight='+focus_height+'&textheight='+text_height+'" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" />');
+	document.write('</object>');
+	
+	// document.write('<object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,0,0" width="100%" height="405">');
+	// document.write('<param name="movie" value="/boloni/images/xshow/focus.swf" />');
+	// document.write('<param name="quality" value="high" />');
+	// document.write('<param name="menu" value="false" />');
+	// document.write('<param name="wmode" value="transparent" />');
+	// document.write('<param name="FlashVars" value="config='+config+'&bcastr_flie='+files+'&bcastr_link='+links+'&bcastr_title='+texts+'" />');
+	// document.write('<embed src="/boloni/images/xshow/focus.swf" wmode="opaque" FlashVars="config='+config+'&bcastr_flie='+files+'&bcastr_link='+links+'&bcastr_title='+texts+'" menu="false" quality="high" width="100%" height="405" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" />');
+	// document.write('</object>');
+	
+	// document.write('<object ID="focus_flash" classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,0,0" width="'+ focus_width +'" height="'+ swf_height +'">');
+	// document.write('<param name="allowScriptAccess" value="always"><param name="movie" value="sitemap/pixviewer.swf"><param name="quality" value="high"><param name="bgcolor" value="#E7E7E7">');
+	// document.write('<param name="menu" value="false"><param name=wmode value="opaque">');
+	// document.write('<param name="FlashVars" value="pics='+pics+'&links='+links+'&texts='+texts+'&borderwidth='+focus_width+'&borderheight='+focus_height+'&textheight='+text_height+'">');
+	// document.write('<embed ID="focus_flash" src="sitemap/pixviewer.swf" wmode="opaque" FlashVars="pics='+pics+'&links='+links+'&texts='+texts+'&borderwidth='+focus_width+'&borderheight='+focus_height+'&textheight='+text_height+'" menu="false" bgcolor="#E7E7E7" quality="high" width="'+ focus_width +'" height="'+ focus_height +'" allowScriptAccess="always" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" />');		document.write('</object>');
 }
 
 
